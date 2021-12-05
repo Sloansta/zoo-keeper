7804245a21f4ec02817a9fde6310363c6889e4b6
Sloan <Sloan.boyce@gmail.com>
2021-12-05 15:38:10 -0500

    got server working withwout errors, prints  json data to localhost

--- data/server.js
+++ data/server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const { animals } = require('./data/animals');
 
-app.get('/api/animals', (req, res) => {
+app.get('/', (req, res) => {
     res.send('Hello!');
 });
 
-app.listen(3001, () => {
-    console.log('API server now on port 3001');
+app.listen(3000, () => {
+    console.log('API server now on port 3000');
 });
