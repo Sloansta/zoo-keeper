f903076a984f4c222573cedf1eaf0a02e6f5a93b
Sloan <Sloan.boyce@gmail.com>
2021-12-06 17:39:37 -0500

    added post and validation to post

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
-const PORT = process.env.PORT || 80;
+const fs = require('fs');
+const path = require('path');
+const { allowedNodeEnvironmentFlags } = require('process');
+const PORT = process.env.PORT || 3000;
 const app = express();
+// parse incoming string or array data
+app.use(express.urlencoded({extended: true}));
+// parse incoming JSON data
+app.use(express.json());
 const { animals } = require('./data/animals');
 
 function filterByQuery(query, animalArray) {
@@ -32,6 +39,29 @@ function findById(id, animalsArray) {
     return result;
 }
 
+function createNewAnimal(body, animalsArray) {
+    console.log(body);
+    const animal = body;
+    animalsArray.push(animal);
+    fs.writeFileSync(path.join(__dirname, './data/animals.json'),
+    JSON.stringify({animals: animalsArray}, null, 2)
+    );
+
+    return body;
+}
+
+function validateAnimal(animal) {
+    if(!animal.name || typeof animal.name !== 'string')
+        return false;
+    if(!animal.species || typeof animal.species !== 'string')
+        return false;
+    if(!animal.diet || typeof animal.diet !== 'string')
+        return false;
+    if(!animal.personalityTraits || !Array.isArray(animal.personalityTraits))
+        return false;
+    return true;
+}
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if(req.query)
@@ -47,6 +77,18 @@ app.get('/api/animals/:id', (req, res) => {
         res.json(result);
     else 
         res.sendStatus(404);
+});
+
+app.post('/api/animals', (req, res) => {
+    // req.body is where our incoming content will be 
+    req.body.id = animals.length.toString();
+
+    if(!validateAnimal(req.body))
+        res.status(400).send('The animal is not properly formatted.');
+    else {
+        const animal = createNewAnimal(req.body, animals);
+        res.json(animal);
+    }
 });
 
 app.listen(PORT, () => {
