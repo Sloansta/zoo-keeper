8c9d69f6d1b744e84733d3d97551666c0489017d
Sloan <Sloan.boyce@gmail.com>
2021-12-05 22:30:28 -0500

    added more basic express functionality

--- server.js
+++ server.js
@@ -2,8 +2,22 @@ const express = require('express');
 const app = express();
 const { animals } = require('./data/animals');
 
+function filterByQuery(query, animalArray) {
+    let filteredResults = animalArray;
+    if(query.diet)
+        filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
+    if(query.species)
+        filteredResults = filteredResults.filter(animal => animal.species === query.species);
+    if(query.name)
+        filteredResults = filteredResults.filter(animal => animal.name === query.name);
+    return filteredResults;
+}
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
+    if(req.query)
+        results = filterByQuery(req.query, results);
+        
     console.log(req.query);
     res.json(results);
 });
